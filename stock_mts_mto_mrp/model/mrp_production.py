0808b0981071f780bb446d0db2e6026d9cbf7736
Arturo Flores <arturo@vauxoo.com>
2018-11-06 06:27:05 +0000

    Revert "fixup! [FIX] stock_mts_mto_rule: fix view"
    This reverts commit 3399ebe2f29fde2121f9501c55812518f865b7b0.

--- stock_mts_mto_mrp/model/mrp_production.py
+++ stock_mts_mto_mrp/model/mrp_production.py
@@ -21,20 +21,15 @@ class MrpProduction(models.Model):
         # This new route is for taking in count this module, the rest is the same as odoo # noqa
         mto_route |= self.env['stock.location.route'].search([
             ('name', 'like', 'Make To Order + Make To Stock')], limit=1)
-        routes = {k: v for d in [
-            {moves.id: moves.product_id.route_ids |
-             moves.product_id.route_from_categ_ids}
-            for moves in self.move_raw_ids] for k, v in d.items()}
-        this_object = self.env['procurement.rule']
-        pulls = [{move.id: this_object.search([
-            ('route_id', 'in', routes.get(move.id).ids),
-            ('location_src_id', '=', move.location_id.id),
-            ('location_id', '=', move.location_dest_id.id)
-        ], limit=1)} for move in self.move_raw_ids]
-        pulls = {k: v for d in pulls for k, v in d.items()}
         for move in self.move_raw_ids:
-            pull = pulls.get(move.id)
+            product = move.product_id
+            routes = product.route_ids | product.route_from_categ_ids
+            # TODO: optimize with read_group?
+            pull = self.env['procurement.rule'].search([
+                ('route_id', 'in', routes.ids),
+                ('location_src_id', '=', move.location_id.id),
+                ('location_id', '=', move.location_dest_id.id)], limit=1)
             if pull.procure_method == 'make_to_order':
                 move.procure_method = pull.procure_method
-            elif not pull and mto_route & routes.get(move.id):  # If there is no make_to_stock rule either # noqa
+            elif not pull and mto_route & routes:  # If there is no make_to_stock rule either # noqa
                 move.procure_method = 'make_to_order'
